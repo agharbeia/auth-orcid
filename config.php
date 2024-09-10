730dbf4b936c79afb093c325421ed6eb19b2d144
Ahmad Gharbeia <ahmad@gharbeia.org>
2024-09-10 12:25:14 +0200

    Update config.php
    replaced "mastodon" with "orcid"
    
    Signed-off-by: Ahmad Gharbeia <ahmad@gharbeia.org>

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
 use humhub\modules\user\authclient\Collection;
-use gm\humhub\modules\auth\mastodon\Module;
-use gm\humhub\modules\auth\mastodon\Events;
+use gm\humhub\modules\auth\orcid\Module;
+use gm\humhub\modules\auth\orcid\Events;
 
 return [
-    'id' => 'auth-mastodon',
+    'id' => 'auth-orcid',
     'class' => Module::class,
-    'namespace' => 'gm\humhub\modules\auth\mastodon',
+    'namespace' => 'gm\humhub\modules\auth\orcid',
     'events' => [
         [Collection::class, Collection::EVENT_AFTER_CLIENTS_SET, [Events::class, 'onAuthClientCollectionInit']]
     ],
